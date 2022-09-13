35d33c6a9d19274d635c6997445e7cbe22a3d032
Jacob Biernat - MACBOOK <jakebiernat@gmail.com>
2022-09-12 23:29:11 -0400

    main page work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentTextComponent } from './content-text/content-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContentTextComponent
   ],
   imports: [
     BrowserModule,
